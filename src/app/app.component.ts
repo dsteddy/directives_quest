bddf70496fe4a4b57903a91453e1d976c7870855
DA_SILVA Teddy <te2dydasilva@gmail.com>
2024-07-08 16:14:09 +0200

    add button changeAdminStatus

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,8 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title: string = 'Bonjour';
-  isAdmin: boolean = true;
+  isAdmin: boolean = false;
+  changeAdminStatus() {
+    this.isAdmin = !this.isAdmin
+  }
 }
