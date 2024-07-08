317dae8b7faf63af06f4c1e6028e41ac9a631a6d
DA_SILVA Teddy <te2dydasilva@gmail.com>
2024-07-08 16:36:04 +0200

    ngClass import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
